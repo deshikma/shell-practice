3db161edd2f4358d8709b77781758fe6cd323f8a
deshikma <mashudhideshik@gmail.com>
2023-09-11 17:48:50 +0530

    msg2

--- gitinstall.sh
+++ gitinstall.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
-# check root user or not
-#if not root user exit the program and inform user to run with sudo access
-#if foor user install mysql 
- 
-
-# $? -->it will store the previous command status
-# $?--> 0 then previous command is success
-# $?--> 1-127 not 0 previous command is failure 
 USERID=$(id -u)
 
-if [ "$USERID" -ne 0 ]
-
+if [ $USERID -NE 0 ]
 then 
-    echo "ERROR :: Please run this script with access"
-    
+    echo "ERROR:: Please run this script with with root access"
+    exit 1
 fi
-
-
 yum install mysql -y
 
 if [ $? -ne 0 ]
-then
-   echo "Installaion of mysql is error"
-   exit 1
-else
-    echo "installation of mysql is success"
+then 
+    echo "installation of mysql is error"
+    exit 1
+else 
+     echo "Installation of mysql is success"
+    
 fi
-
-
 
