d4c48619557eb5346970a1b0bbabd972b097f10c
deshikma <mashudhideshik@gmail.com>
2023-09-11 15:52:06 +0530

    msg

--- variables.sh
+++ variables.sh
@@ -2,8 +2,17 @@
  NUMBER1=$1
  NUMBER2=$2
  NUMBER3=$3
- Number4=$4
- echo $((NUMBER1+NUMBER2))
- echo $((NUMBER3-NUMBER4))
+ NUMBER4=$4
+ number5=$5
+ number6=$6
+
+ Sum=$((NUMBER1+NUMBER2))
+ sub=$((NUMBER3-NUMBER4))
+ mul=$((number5*number6))
+
+ echo "adition of two numbers is $Sum"
+ echo "substraction of two number is $sub" 
+ echo "multiplication of two numbers is $mul"
+
 
  
